6a45513d84e2181db35f0c3c337b068b4dca7ab9
corey yung <coreyrayburnyung@gmail.com>
2020-10-06 16:03:16 -0500

    still expanding Outline coverage

--- sourdough/project/editor.py
+++ sourdough/project/editor.py
@@ -72,6 +72,9 @@ class Outline(sourdough.Lexicon):
     """
     contents: Mapping[str, Details] = dataclasses.field(default_factory = dict)
     project: sourdough.Project = dataclasses.field(repr = False, default = None) 
+    _bases: Mapping[str, str] = dataclasses.field(
+        repr = False, 
+        default_factory = dict)
 
     """ Dunder Methods """
     
@@ -100,86 +103,67 @@ class Draft(sourdough.Stage):
         Returns:
             sourdough.Outline: [description]
             
-        """       
-        suffixes = tuple(project.hierarchy.keys())
-        outline  = self._create_outline(project = project, suffixes = suffixes)
+        """ 
+        outline = Outline(project = project)
+        for name in project.settings.key():     
+            outline = self._process_section(
+                name = name,
+                outline = outline,
+                project = project)
         project.design = outline
         print('test project outline', project.design)
         return project
         
     """ Private Methods """
     
-    def _create_outline(self, project: sourdough.Project, 
-                        suffixes: Tuple[str]) -> Outline:
-        """[summary]
-
-        Args:
-            project (sourdough.Project): [description]
-            suffixes (Tuple[str]): [description]
-
-        Returns:
-            Outline: [description]
-            
-        """
-        bases = {}
-        outline = Outline(project = project)
-        for section in project.settings.values():
-            outline[section] = Details(name = section)
-            outline = self._process_section(
-                section = section, 
-                outline = outline,
-                project = project)
-            bases.update(self._add_bases(details = outline[section]))
-            outline[section].base = bases[section]
-        return outline
-
-    def _process_section(self, section: Mapping[str, Any], outline: Outline,
-                         project: sourdough.Project) -> Dict[str, Details]:
-        # contents = []
-        # name = None
-        # base = None
-        # contains = None
-        # design = None
-        # parameters = {}
-        # attributes = {}
-        
-        for key, value in section.items():
+    def _process_section(self, name: str, outline: Outline, 
+                         project: sourdough.Project) -> Outline:
+        if name not in outline:
+            outline[name] = Details(name = name)   
+        for key, value in outline[name].items():
             if key.endswith(tuple(project.hierarchy.keys())):
-                details.contents = sourdough.tools.listify(value)
-                outline = self._process_value()
-                name, suffix = self._divide_key(key = key)
-                contains = suffix.rstrip('s')
+                key_name, key_suffix = self._divide_key(key = key)
+                kind = key_suffix.rstrip('s')
+                contents = sourdough.tools.listify(value)
+                if key_name == name:
+                    outline[name].contents = contents
+                    outline[name].contains = kind
+                for item in contents:
+                    outline = self._process_components(
+                        name = key_name,
+                        base = kind,
+                        contents = contents,
+                        outline = outline)
             elif key.endswith('design'):
-                details.design = value
+                outline[name].design = value
             else:
-                details.attributes[key] = value
+                outline[name].attributes[key] = value
+        return outline
+    
+    def _process_components(self, name: str, base: str, 
+                            contents: Mapping[str, Sequence[str]],
+                            outline: Outline) -> Outline:
+        return outline
         
-    def _process_value(self, name: str, suffix: str) -> Dict[str, Details]:
-        
-        
-        
-                
     def _get_details(self, key: str, value: Sequence[str],
-                        suffixes: Sequence[str],
-                        project: sourdough.Project) -> Details:
+                     project: sourdough.Project) -> Details:
         """[summary]
 
         Args:
             key (str): [description]
             value (Sequence[str]): [description]
-            suffixes (Sequence[str]): [description]
             project (sourdough.Project): [description]
 
         Returns:
             Details: [description]
+            
         """
-        name, suffix = self._divide_key(key = key)
-        contains = suffix.rstrip('s')
+        
+        
         design = self._get_design(name = name, project = project)
         attributes = self._get_attributes(
             name = name, 
-            project = project,
-            suffixes = suffixes)
+            project = project)
         contents = sourdough.tools.listify(value)
         details = Details(
             name = name,
@@ -188,7 +172,23 @@ class Draft(sourdough.Stage):
             contents = contents,
             attributes = attributes)
         return details
-    
+        
+    def _process_value(self, name: str, contents: Sequence[str],
+                       outline: Outline) -> Outline:
+        for item in contents:
+            outline[item].contents = contents
+            outline = self._process_value()
+            name, suffix = self._divide_key(key = key)
+            contains = suffix.rstrip('s')
+        
+        
+ 
+    def _add_bases(details: Details) -> Dict[str, str]:
+        bases = {}
+        for item in details.contents:
+            bases[item] = details.contains
+        return bases
+
     def _divide_key(self, key: str) -> Sequence[str, str]:
         """[summary]
 
@@ -220,14 +220,13 @@ class Draft(sourdough.Stage):
             structure = 'pipeline'
         return structure
 
-    def _get_attributes(self, name: str, project: sourdough.Project,
-                        suffixes: Tuple[str]) -> Mapping[str, Any]:
+    def _get_attributes(self, name: str, 
+                        project: sourdough.Project) -> Mapping[str, Any]:
         """[summary]
 
         Args:
             name (str): [description]
             project (sourdough.Project): [description]
-            suffixes (Tuple[str]):
 
         Returns:
             Mapping[str, Any]: [description]
@@ -236,7 +235,7 @@ class Draft(sourdough.Stage):
         attributes = {}
         try:
             for key, value in project.settings[name].items():
-                if not (key.endswith(suffixes) 
+                if not (key.endswith(tuple(project.hierarchy.keys())) 
                         or key.endswith('_design')
                         or key.endswith('workflow')):
                     attributes[key] = value
